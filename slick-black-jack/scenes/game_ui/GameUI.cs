afe1ddb49f80b374e8288d9b37677dba1d832f5c
AndrewOakley <98728701+AndrewOakley@users.noreply.github.com>
2026-01-31 21:16:54 -0600

    add npc chip counts

--- slick-black-jack/scenes/game_ui/GameUI.cs
+++ slick-black-jack/scenes/game_ui/GameUI.cs
@@ -8,9 +8,16 @@ public partial class GameUI : Control {
 	[Signal] public delegate void DoubleDownEventHandler();
 
 	private Label _playerChipCountLabel;
+	private Label _npcOneChipsLabel;
+	private Label _npcTwoChipsLabel;
+
+	private Player _npcOne;
+	private Player _npcTwo;
 	
 	public override void _Ready() {
 		_playerChipCountLabel = GetNode<Label>("%PlayerChipCount");
+		_npcOneChipsLabel = GetNode<Label>("%NpcOneChipCount");
+		_npcTwoChipsLabel = GetNode<Label>("%NpcTwoChipCount");
 
 		var players = GetTree().GetNodesInGroup("main_player");
 		foreach (var player in players) {
@@ -19,6 +26,17 @@ public partial class GameUI : Control {
 				UpdatePlayerChipCount(mainPlayer.Chips);
 			}
 		}
+		
+		var npcs = GetTree().GetNodesInGroup("npc_player");
+		if (npcs.Count < 2) {
+			throw new Exception("Expected at least 2 NPC players in scene");
+		};
+		
+		_npcOne = npcs[0] as Player;
+		_npcTwo = npcs[1] as Player;
+		
+		_npcOne.ChipsChanged += UpdateNpcOneChipCount;
+		_npcTwo.ChipsChanged += UpdateNpcTwoChipCount;
 	}
 
 	public void OnHitPressed() {
@@ -44,5 +62,22 @@ public partial class GameUI : Control {
 	
 	private void UpdatePlayerChipCount(int chipCount) {
 		_playerChipCountLabel.Text = "Current: " + chipCount.ToString();
+	}
+
+	private void UpdateNpcOneChipCount(int chipCount) {
+		UpdateNpcChipCount(0, chipCount);
+	}
+	
+	private void UpdateNpcTwoChipCount(int chipCount) {
+		UpdateNpcChipCount(1, chipCount);
+	}
+	
+	private void UpdateNpcChipCount(int npcIndex, int chipCount) {
+		if (npcIndex == 0) {
+			_npcOneChipsLabel.Text = $"{_npcOne.Name}: " + chipCount.ToString();
+		}
+		else if (npcIndex == 1) {
+			_npcTwoChipsLabel.Text = $"{_npcTwo.Name}: " + chipCount.ToString();
+		}
 	}
 }
