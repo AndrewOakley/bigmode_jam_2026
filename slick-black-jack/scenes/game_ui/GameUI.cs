f6c008fbb81f4a8799b93fa5528cd53b8d7eee14
Elias Tracy <elitracy19@gmail.com>
2026-02-05 19:52:58 -0600

    feat: updated ui

--- slick-black-jack/scenes/game_ui/GameUI.cs
+++ slick-black-jack/scenes/game_ui/GameUI.cs
@@ -3,208 +3,209 @@ using System;
 using SlickBlackJack.Components;
 
 public partial class GameUI : Control {
-	[Signal] public delegate void HitEventHandler();
-	[Signal] public delegate void SplitEventHandler();
-	[Signal] public delegate void StandEventHandler();
-	[Signal] public delegate void DoubleDownEventHandler();
-	
-	[Export] private bool _enableTimer = true;
-	[Export] private int _timeLimit = 15;
-	[Export] public int BetStep = 10; 
-	
-	private Label _playerChipCountLabel;
-	private Label _npcOneChipsLabel;
-	private Label _npcTwoChipsLabel;
-	private Label _handTimerLabel;
-	private Label _betAmountLabel;
-	private Label _minBetLabel;
-	private Label _maxBetLabel;
-	private BetSlider _betSlider;
-	private Button _placeBetButton;
+    [Signal] public delegate void HitEventHandler();
+    [Signal] public delegate void SplitEventHandler();
+    [Signal] public delegate void StandEventHandler();
+    [Signal] public delegate void DoubleDownEventHandler();
 
-	private Player _mainPlayer;
-	private Player _npcOne;
-	private Player _npcTwo;
+    [Export] private bool _enableTimer = true;
+    [Export] private int _timeLimit = 15;
+    [Export] public int BetStep = 10;
 
-	private Timer _handTimer;
-	private int _timeRemaining;
-	
-	public override void _Ready() {
-		_playerChipCountLabel = GetNode<Label>("%PlayerChipCount");
-		_npcOneChipsLabel = GetNode<Label>("%NpcOneChipCount");
-		_npcTwoChipsLabel = GetNode<Label>("%NpcTwoChipCount");
-		_handTimerLabel = GetNode<Label>("%HandTimer");
-		_handTimerLabel.Hide();
+    private Label _playerChipCountLabel;
+    private Label _npcOneChipsLabel;
+    private Label _npcTwoChipsLabel;
+    private Label _handTimerLabel;
+    private Label _betAmountLabel;
+    private Label _minBetLabel;
+    private Label _maxBetLabel;
+    private BetSlider _betSlider;
+    private Button _placeBetButton;
 
-		_betAmountLabel = GetNode<Label>("%BetAmount");
-		_minBetLabel = GetNode<Label>("%MinBet");
-		_maxBetLabel = GetNode<Label>("%MaxBet");
-		_betSlider = GetNode<BetSlider>("%BetSlider");
-		_betSlider.ValueChanged += OnBetValueChanged;
+    private Player _mainPlayer;
+    private Player _npcOne;
+    private Player _npcTwo;
 
-		_placeBetButton = GetNode<Button>("%PlaceBet");
-		_placeBetButton.Hide();
+    private Timer _handTimer;
+    private int _timeRemaining;
 
-		_handTimer = new Timer();
-		_handTimer.WaitTime = 1.0;
-		_handTimer.OneShot = false;
-		_handTimer.Timeout += OnTimerTick;
-		AddChild(_handTimer);
+    public override void _Ready() {
+        _playerChipCountLabel = GetNode<Label>("%PlayerChipCount");
+        _npcOneChipsLabel = GetNode<Label>("%NpcOneChipCount");
+        _npcTwoChipsLabel = GetNode<Label>("%NpcTwoChipCount");
+        _handTimerLabel = GetNode<Label>("%HandTimer");
+        _handTimerLabel.Hide();
 
-		var mainPlayers = GetTree().GetNodesInGroup("main_player");
-		foreach (var player in mainPlayers) {
-			if (player is Player mainPlayer) {
-				_mainPlayer = mainPlayer;
-				_mainPlayer.ChipsChanged += UpdatePlayerChipCount;
-				UpdatePlayerChipCount(_mainPlayer.Chips);
-				_mainPlayer.HandStarted += OnMainHandStarted;
-				_mainPlayer.PlayerTurnEnded += OnMainTurnEnded;
-			}
-		}
+        _betAmountLabel = GetNode<Label>("%BetAmount");
+        _minBetLabel = GetNode<Label>("%MinBet");
+        _maxBetLabel = GetNode<Label>("%MaxBet");
+        _betSlider = GetNode<BetSlider>("%BetSlider");
+        _betSlider.ValueChanged += OnBetValueChanged;
 
-		var npcs = GetTree().GetNodesInGroup("npc_player");
-		if (npcs.Count < 2) {
-			throw new Exception("Expected at least 2 NPC players in scene");
-		};
-		
-		_npcOne = npcs[0] as Player;
-		_npcTwo = npcs[1] as Player;
-		
-		_npcOne.ChipsChanged += UpdateNpcOneChipCount;
-		_npcTwo.ChipsChanged += UpdateNpcTwoChipCount;
-		_mainPlayer.ChipsChanged += OnChipsChanged;
-		
-		OnChipsChanged(_mainPlayer.Chips);
-		
-		Utils.StopTurnTimer += OnStopTurnTimer;
-		Utils.BettingStarted += OnBettingStarted;
-	}
+        _placeBetButton = GetNode<Button>("%PlaceBet");
+        _placeBetButton.Hide();
 
-	private void OnChipsChanged(int chips) {
-		var maxBet = Mathf.Clamp(chips, 0, _mainPlayer.Chips);
-		var minBet = Mathf.Clamp(chips, 0, BetStep);
-		
-		_betSlider.MaxValue = maxBet;
-		_betSlider.MinValue = minBet;
-		_maxBetLabel.Text = "$"  + maxBet.ToString("N0");
-		_minBetLabel.Text = "$" + minBet.ToString("N0");
-		
-		_betSlider.Value = Math.Min(RoundBet(_mainPlayer.PlayerBet, minBet, maxBet), maxBet);
-		_mainPlayer.PlayerBet = Mathf.Clamp(_mainPlayer.PlayerBet, minBet, maxBet);
-		
-		OnBetValueChanged(_mainPlayer.PlayerBet);
-	}
+        _handTimer = new Timer();
+        _handTimer.WaitTime = 1.0;
+        _handTimer.OneShot = false;
+        _handTimer.Timeout += OnTimerTick;
+        AddChild(_handTimer);
 
-	private int RoundBet(int bet, int minBet, int maxBet) {
-		int rounded = (int)(Math.Floor(bet / (float)BetStep) * BetStep);
-		return Math.Clamp(rounded, minBet, maxBet);
-	}
-	
-	private void OnBetValueChanged(int value) {
-		var maxBet = Mathf.Clamp(value, 0, _mainPlayer.Chips);
-		var minBet = Mathf.Clamp(value, 0, BetStep);
-		_mainPlayer.PlayerBet = Math.Min(RoundBet(value, minBet, maxBet),
-			_betSlider.MaxValue);
-		_betAmountLabel.Text = $"CURRENT BET:\n${_mainPlayer.PlayerBet:N0}";
-	}
-	
-	// ALWAYS DO THIS IF CALLING SIGNALS FROM UTILS
-	protected override void Dispose(bool disposing) {
-		Utils.StopTurnTimer -= OnStopTurnTimer;
-		Utils.BettingStarted -= OnBettingStarted;
-		base.Dispose(disposing);
-	}
+        var mainPlayers = GetTree().GetNodesInGroup("main_player");
+        foreach (var player in mainPlayers) {
+            if (player is Player mainPlayer) {
+                _mainPlayer = mainPlayer;
+                _mainPlayer.ChipsChanged += UpdatePlayerChipCount;
+                UpdatePlayerChipCount(_mainPlayer.Chips);
+                _mainPlayer.HandStarted += OnMainHandStarted;
+                _mainPlayer.PlayerTurnEnded += OnMainTurnEnded;
+            }
+        }
 
-	private void OnStopTurnTimer() {
-		StopTimer();
-	}
-	
-	private void OnBettingStarted() {
-		_placeBetButton.Show();
-	}
-	
-	private void OnPlaceBetPressed() {
-		Utils.EmitPlayerbetSubmitted(_mainPlayer.PlayerBet);
-		_placeBetButton.Hide();
-	}
+        var npcs = GetTree().GetNodesInGroup("npc_player");
+        if (npcs.Count < 2) {
+            throw new Exception("Expected at least 2 NPC players in scene");
+        }
+        ;
 
-	public void OnHitPressed() {
-		EmitSignal(SignalName.Hit);
-	}
-	
-	public void OnSplitPressed() {
-		EmitSignal(SignalName.Split);
-	}
-	
-	public void OnStandPressed() {
-		EmitSignal(SignalName.Stand);
-	}
-	
-	public void OnDoubleDownPressed() {
-		EmitSignal(SignalName.DoubleDown);
-	}
+        _npcOne = npcs[0] as Player;
+        _npcTwo = npcs[1] as Player;
 
-	public void OnRestartPressed() {
+        _npcOne.ChipsChanged += UpdateNpcOneChipCount;
+        _npcTwo.ChipsChanged += UpdateNpcTwoChipCount;
+        _mainPlayer.ChipsChanged += OnChipsChanged;
+
+        OnChipsChanged(_mainPlayer.Chips);
+
+        Utils.StopTurnTimer += OnStopTurnTimer;
+        Utils.BettingStarted += OnBettingStarted;
+    }
+
+    private void OnChipsChanged(int chips) {
+        var maxBet = Mathf.Clamp(chips, 0, _mainPlayer.Chips);
+        var minBet = Mathf.Clamp(chips, 0, BetStep);
+
+        _betSlider.MaxValue = maxBet;
+        _betSlider.MinValue = minBet;
+        _maxBetLabel.Text = "$" + maxBet.ToString("N0");
+        _minBetLabel.Text = "$" + minBet.ToString("N0");
+
+        _betSlider.Value = Math.Min(RoundBet(_mainPlayer.PlayerBet, minBet, maxBet), maxBet);
+        _mainPlayer.PlayerBet = Mathf.Clamp(_mainPlayer.PlayerBet, minBet, maxBet);
+
+        OnBetValueChanged(_mainPlayer.PlayerBet);
+    }
+
+    private int RoundBet(int bet, int minBet, int maxBet) {
+        int rounded = (int)(Math.Floor(bet / (float)BetStep) * BetStep);
+        return Math.Clamp(rounded, minBet, maxBet);
+    }
+
+    private void OnBetValueChanged(int value) {
+        var maxBet = Mathf.Clamp(value, 0, _mainPlayer.Chips);
+        var minBet = Mathf.Clamp(value, 0, BetStep);
+        _mainPlayer.PlayerBet = Math.Min(RoundBet(value, minBet, maxBet),
+            _betSlider.MaxValue);
+        _betAmountLabel.Text = $"BET: ${_mainPlayer.PlayerBet:N0}";
+    }
+
+    // ALWAYS DO THIS IF CALLING SIGNALS FROM UTILS
+    protected override void Dispose(bool disposing) {
+        Utils.StopTurnTimer -= OnStopTurnTimer;
+        Utils.BettingStarted -= OnBettingStarted;
+        base.Dispose(disposing);
+    }
+
+    private void OnStopTurnTimer() {
+        StopTimer();
+    }
+
+    private void OnBettingStarted() {
+        _placeBetButton.Show();
+    }
+
+    private void OnPlaceBetPressed() {
+        Utils.EmitPlayerbetSubmitted(_mainPlayer.PlayerBet);
+        _placeBetButton.Hide();
+    }
+
+    public void OnHitPressed() {
+        EmitSignal(SignalName.Hit);
+    }
+
+    public void OnSplitPressed() {
+        EmitSignal(SignalName.Split);
+    }
+
+    public void OnStandPressed() {
+        EmitSignal(SignalName.Stand);
+    }
+
+    public void OnDoubleDownPressed() {
+        EmitSignal(SignalName.DoubleDown);
+    }
+
+    public void OnRestartPressed() {
         GD.Print("Restarting...");
-		GetTree().ReloadCurrentScene();
-	}
-	
-	public void OnCheatPressed() {
-		Utils.EmitCheatStarted();
-	}
-	
-	public void StopTimer() {
-		_handTimer.Stop();
-		_handTimerLabel.Hide();
-	}
-	
-	private void UpdatePlayerChipCount(int chipCount) {
-		_playerChipCountLabel.Text = "$" + chipCount.ToString("N0");
-	}
+        GetTree().ReloadCurrentScene();
+    }
 
-	private void UpdateNpcOneChipCount(int chipCount) {
-		UpdateNpcChipCount(0, chipCount);
-	}
-	
-	private void UpdateNpcTwoChipCount(int chipCount) {
-		UpdateNpcChipCount(1, chipCount);
-	}
-	
-	private void OnMainHandStarted(Hand hand) {
-		if (!_enableTimer) return;
-		
-		StopTimer();
+    public void OnCheatPressed() {
+        Utils.EmitCheatStarted();
+    }
 
-		_handTimerLabel.Show();
-		_timeRemaining = _timeLimit;
-		_handTimerLabel.Text = _timeRemaining.ToString();
-		_handTimer.Start();
-	}
+    public void StopTimer() {
+        _handTimer.Stop();
+        _handTimerLabel.Hide();
+    }
 
-	private void OnTimerTick() {
-		_timeRemaining--;
-		_handTimerLabel.Text = _timeRemaining.ToString();
+    private void UpdatePlayerChipCount(int chipCount) {
+        _playerChipCountLabel.Text = "$" + chipCount.ToString("N0");
+    }
 
-		if (_timeRemaining <= 0 && !_handTimer.IsStopped()) {
-			StopTimer();
-			_handTimerLabel.Hide();
-			Utils.EmitTurnTimerExpired();
-			GD.Print("Turn timer expired");
-		}
-	}
-	
-	private void OnMainTurnEnded() {
-		if (!_enableTimer) return;
-		
-		StopTimer();
-	}
-	
-	private void UpdateNpcChipCount(int npcIndex, int chipCount) {
-		if (npcIndex == 0) {
-			_npcOneChipsLabel.Text = $"{_npcOne.Name}: " + chipCount.ToString();
-		}
-		else if (npcIndex == 1) {
-			_npcTwoChipsLabel.Text = $"{_npcTwo.Name}: " + chipCount.ToString();
-		}
-	}
+    private void UpdateNpcOneChipCount(int chipCount) {
+        UpdateNpcChipCount(0, chipCount);
+    }
+
+    private void UpdateNpcTwoChipCount(int chipCount) {
+        UpdateNpcChipCount(1, chipCount);
+    }
+
+    private void OnMainHandStarted(Hand hand) {
+        if (!_enableTimer) return;
+
+        StopTimer();
+
+        _handTimerLabel.Show();
+        _timeRemaining = _timeLimit;
+        _handTimerLabel.Text = _timeRemaining.ToString();
+        _handTimer.Start();
+    }
+
+    private void OnTimerTick() {
+        _timeRemaining--;
+        _handTimerLabel.Text = _timeRemaining.ToString();
+
+        if (_timeRemaining <= 0 && !_handTimer.IsStopped()) {
+            StopTimer();
+            _handTimerLabel.Hide();
+            Utils.EmitTurnTimerExpired();
+            GD.Print("Turn timer expired");
+        }
+    }
+
+    private void OnMainTurnEnded() {
+        if (!_enableTimer) return;
+
+        StopTimer();
+    }
+
+    private void UpdateNpcChipCount(int npcIndex, int chipCount) {
+        if (npcIndex == 0) {
+            _npcOneChipsLabel.Text = $"{_npcOne.Name}: " + chipCount.ToString();
+        }
+        else if (npcIndex == 1) {
+            _npcTwoChipsLabel.Text = $"{_npcTwo.Name}: " + chipCount.ToString();
+        }
+    }
 }
